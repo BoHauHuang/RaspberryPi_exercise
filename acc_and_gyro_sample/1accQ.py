4eb553bb7dbbf34cf214c9217dd5ca00580d1199
bohauhuang <xab9604030925@gmail.com>
2020-03-27 14:06:03 +0800

    complete quiz 1 and 2

--- acc_and_gyro_sample/1accQ.py
+++ acc_and_gyro_sample/1accQ.py
@@ -127,9 +127,9 @@ class ADXL345(IMU):
         aY = self.getYg()
         aZ = self.getZg()
         try:
-        	self.roll = atan(aY/aZ)/pi*180
+            self.roll = atan(aY/aZ)/pi*180
         except:
-        	self.roll = -999
+            self.roll = -999
         return self.roll
 
     def getTilt(self):
@@ -137,39 +137,39 @@ class ADXL345(IMU):
         aY = self.getYg()
         aZ = self.getZg()
         try:
-        	self.tilt = acos(aZ/sqrt(pow(aX,2)+pow(aY,2)+pow(aZ,2)))/pi*180
+            self.tilt = acos(aZ/sqrt(pow(aX,2)+pow(aY,2)+pow(aZ,2)))/pi*180
         except:
-        	self.tile = -999
-    	return self.tilt
+            self.tile = -999
+        return self.tilt
 
     def getNorm(self, ax, ay, az):
-    	self.norm = sqrt(pow(ax, 2)+pow(ay, 2)+pow(az, 2))
-    	return self.norm
+        self.norm = sqrt(pow(ax, 2)+pow(ay, 2)+pow(az, 2))
+        return self.norm
 
 try:
     sensors = gy801()
     adxl345 = sensors.accel
-	while 1:
-	    adxl345.getX()
-	    adxl345.getY()
-	    adxl345.getZ()
+    while 1:
+        adxl345.getX()
+        adxl345.getY()
+        adxl345.getZ()
 
-	    print ("ACC: ")
-	    print ("X = %.3f m/s2" % ( adxl345.X ))
-	    print ("Y = %.3f m/s2" % ( adxl345.Y ))
-	    print ("Z = %.3f m/s2" % ( adxl345.Z ))
-	#    print ("x = %.3fG" % ( adxl345.Xg ))
-	#    print ("y = %.3fG" % ( adxl345.Yg ))
-	#    print ("z = %.3fG" % ( adxl345.Zg ))
-	    print ("Xraw = %.3f" % ( adxl345.Xraw ))
-	    print ("Yraw = %.3f" % ( adxl345.Yraw ))
-	    print ("Zraw = %.3f" % ( adxl345.Zraw ))
+        print ("ACC: ")
+        print ("X = %.3f m/s2" % ( adxl345.X ))
+        print ("Y = %.3f m/s2" % ( adxl345.Y ))
+        print ("Z = %.3f m/s2" % ( adxl345.Z ))
+    #    print ("x = %.3fG" % ( adxl345.Xg ))
+    #    print ("y = %.3fG" % ( adxl345.Yg ))
+    #    print ("z = %.3fG" % ( adxl345.Zg ))
+        print ("Xraw = %.3f" % ( adxl345.Xraw ))
+        print ("Yraw = %.3f" % ( adxl345.Yraw ))
+        print ("Zraw = %.3f" % ( adxl345.Zraw ))
 
-	    print ("norm = %.3f" %( adxl345.getNorm(adxl345.X, adxl345.Y, adxl345.Z) ))
+        print ("norm = %.3f" %( adxl345.getNorm(adxl345.X, adxl345.Y, adxl345.Z) ))
 
-	    print ("pitch = %.3f" % ( adxl345.getPitch() ))
-	    print ("roll = %.3f" % ( adxl345.getRoll() ))
-	    print ("tilt = %.3f" %( adxl345.getTilt() ))
+        print ("pitch = %.3f" % ( adxl345.getPitch() ))
+        print ("roll = %.3f" % ( adxl345.getRoll() ))
+        print ("tilt = %.3f" %( adxl345.getTilt() ))
 
 except KeyboardInterrupt:
     print("Cleanup")
