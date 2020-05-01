f9a72e42e2c1d545ebe85a01d13235e3218843c0
bohauhuang <xab9604030925@gmail.com>
2020-05-01 13:26:39 +0800

    update lab 6

---
+++ face detection/camera_pi.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+# Copyright (c) 2014, raspberrypi.com.tw
+# All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Author : sosorry
+# Date   : 05/31/2015
+# Origin : http://blog.miguelgrinberg.com/post/video-streaming-with-flask
+
+from imutils.video.pivideostream import PiVideoStream
+import cv2
+
+cascPath = "model/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+class Camera(object):
+    def __init__(self):
+        if cv2.__version__.startswith('2'):
+            PROP_FRAME_WIDTH = cv2.cv.CV_CAP_PROP_FRAME_WIDTH
+            PROP_FRAME_HEIGHT = cv2.cv.CV_CAP_PROP_FRAME_HEIGHT
+        elif cv2.__version__.startswith('3'):
+            PROP_FRAME_WIDTH = cv2.CAP_PROP_FRAME_WIDTH
+            PROP_FRAME_HEIGHT = cv2.CAP_PROP_FRAME_HEIGHT
+        self.video = cv2.VideoCapture(0)
+        #self.video = PiVideoStream().start()
+        self.img = None
+
+
+    def __del__(self):
+        self.video.release()
+    def get_img(self):
+        while True:
+            image = self.video.read()
+            if image is not None:
+                return image
+
+    def get_frame(self):
+        self.img = self.get_img()
+
+        gray_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray_img,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
+
+        print ("Found {0} faces!".format(len(faces)))
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(self.img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        ret, jpeg = cv2.imencode('.jpg', self.img)
+        return jpeg.tostring()
