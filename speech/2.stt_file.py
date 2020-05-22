bb0723c2b4ff6e0e1e585621d3e2d29a3536f500
bohauhuang <nctu0511097@gmail.com>
2020-05-22 15:22:48 +0800

    update lab 9

--- speech/2.stt_file.py
+++ speech/2.stt_file.py
@@ -6,7 +6,7 @@ from gtts import gTTS
 r=sr.Recognizer() 
 
 #myvoice = sr.AudioFile('hello.mp3')
-myvoice = sr.AudioFile('ls.wav')
+myvoice = sr.AudioFile('proc.wav')
 with myvoice as source:
     print("Use audio file as input!")
     audio = r.record(source)
@@ -16,9 +16,9 @@ try:
     print("Google Speech Recognition thinks you said:")
     cmd = r.recognize_google(audio)
     print(cmd)
-    if cmd == "files":
-        os.system('ls')
-        output = os.popen('ls').read()
+    if cmd == "process":
+        os.system('ps')
+        output = os.popen('ps').read()
         tts = gTTS(text=output, lang='en')
         tts.save('output.mp3')
         os.system('omxplayer -o local -p output.mp3 > /dev/null 2>&1')
--- speech/3.tts.py
+++ speech/3.tts.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import os
 
-tts = gTTS(text='ls', lang='en')
-tts.save('hello.wav')
+tts = gTTS(text='process', lang='en')
+tts.save('proc.wav')
 
-os.system('omxplayer -o local -p hello.wav > /dev/null 2>&1')
+os.system('omxplayer -o local -p proc.wav > /dev/null 2>&1')
